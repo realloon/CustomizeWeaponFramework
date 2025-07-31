1c954764022f8ee64a2a7dbc66bd73023e0477ea
sincerajn <sincerajn@outlook.com>
2025-08-01 00:49:36 +0800

    feat: remove unused field

--- DefModExtension/TraitModuleExtension.cs
+++ DefModExtension/TraitModuleExtension.cs
@@ -9,7 +9,6 @@ public class TraitModuleExtension : DefModExtension {
     public WeaponTraitDef weaponTraitDef;
     public Part part;
 
-    public List<WeaponSpecificOffset> offsets;
     public List<ThingDef> requiredWeaponDefs;
     public List<string> requiredWeaponTags;
     public List<ThingDef> excludeWeaponDefs;

2f6cda33af282fb003228e8fedc3646c8b1091e8
sincerajn <sincerajn@outlook.com>
2025-08-18 11:41:22 +0800

    feat: Enhance tooltip to include trait effect lines

--- ThingComps/CompTraitModule.cs
+++ ThingComps/CompTraitModule.cs
@@ -1,4 +1,6 @@
-// using RimWorld;
+using System.Text;
+using RimWorld;
+using UnityEngine;
 using Verse;
 
 namespace CWF;
@@ -6,16 +8,91 @@ namespace CWF;
 public class CompTraitModule : ThingComp {
     // private CompProperties_TraitModule Props => (CompProperties_TraitModule)props;
 
-    // private WeaponTraitDef _cachedTraitDef;
-    // private bool _isTraitDefCached;
+    private WeaponTraitDef _cachedTraitDef;
+    private bool _isTraitDefCached;
 
-    // private WeaponTraitDef TraitDef {
-    //     get {
-    //         if (_isTraitDefCached) return _cachedTraitDef;
-    //
-    //         _cachedTraitDef = parent.def.GetModExtension<TraitModuleExtension>()?.weaponTraitDef;
-    //         _isTraitDefCached = true;
-    //         return _cachedTraitDef;
-    //     }
-    // }
+    private Part _cachedPart;
+    private bool _isPartCached;
+
+    private WeaponTraitDef TraitDef {
+        get {
+            if (_isTraitDefCached) return _cachedTraitDef;
+
+            _cachedTraitDef = parent.def.GetModExtension<TraitModuleExtension>()?.weaponTraitDef;
+            _isTraitDefCached = true;
+            return _cachedTraitDef;
+        }
+    }
+
+    private Part Part {
+        get {
+            if (_isPartCached) return _cachedPart;
+
+            _cachedPart = parent.def.GetModExtension<TraitModuleExtension>().part;
+            _isPartCached = true;
+            return _cachedPart;
+        }
+    }
+
+    public override IEnumerable<StatDrawEntry> SpecialDisplayStats() {
+        if (TraitDef == null) {
+            Log.Warning($"[CWF]: {parent.def.defName} no TraitModuleExtension or its weaponTraitDef is null.");
+            yield break;
+        }
+
+        var sb = new StringBuilder();
+        var effectLines = GetTraitEffectLines(TraitDef);
+
+        if (effectLines.Count > 0) {
+            sb.AppendLine($"CWF_UI_ModuleEffectsDesc".Translate(TraitDef.Named("MODULE")) + ":");
+            sb.AppendLine();
+            sb.AppendLine(string.Join("\n", effectLines));
+            sb.AppendLine();
+        }
+
+        sb.AppendLine("CWF_UI_PartOf".Translate() + ": " + $"CWF_UI_{Part}".Translate());
+
+        yield return new StatDrawEntry(
+            StatCategoryDefOf.BasicsImportant,
+            "CWF_UI_ModuleEffects".Translate(),
+            TraitDef.LabelCap,
+            sb.ToString().TrimEndNewlines(),
+            1000
+        );
+    }
+
+    public static List<string> GetTraitEffectLines(WeaponTraitDef traitDef) {
+        if (traitDef == null) return new List<string>();
+
+        var effectLines = new List<string>();
+
+        // offset
+        if (!traitDef.statOffsets.NullOrEmpty()) {
+            effectLines.AddRange(traitDef.statOffsets.Select(offset =>
+                $" - {offset.stat.LabelCap}: {offset.stat.Worker.ValueToString(offset.value, false, ToStringNumberSense.Offset)}"));
+        }
+
+        // factor
+        if (!traitDef.statFactors.NullOrEmpty()) {
+            effectLines.AddRange(traitDef.statFactors.Select(factor =>
+                $" - {factor.stat.LabelCap}: {factor.stat.Worker.ValueToString(factor.value, false, ToStringNumberSense.Factor)}"));
+        }
+
+        if (!Mathf.Approximately(traitDef.burstShotCountMultiplier, 1f)) {
+            effectLines.Add(
+                $" - {"CWF_BurstShotCountFactor".Translate()}: {traitDef.burstShotCountMultiplier.ToStringByStyle(ToStringStyle.PercentZero, ToStringNumberSense.Factor)}");
+        }
+
+        if (!Mathf.Approximately(traitDef.burstShotSpeedMultiplier, 1f)) {
+            effectLines.Add(
+                $" - {"CWF_TicksBetweenBurstShotsFactor".Translate()}: {traitDef.burstShotSpeedMultiplier.ToStringByStyle(ToStringStyle.PercentZero, ToStringNumberSense.Factor)}");
+        }
+
+        if (!Mathf.Approximately(traitDef.additionalStoppingPower, 0.0f)) {
+            effectLines.Add(
+                $" - {"StatsReport_AdditionalStoppingPower".Translate()} {traitDef.additionalStoppingPower.ToStringByStyle(ToStringStyle.FloatOne, ToStringNumberSense.Offset)}");
+        }
+
+        return effectLines;
+    }
 }

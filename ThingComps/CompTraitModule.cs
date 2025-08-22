503524d911d0f7fb80e243dad836d89ae495090a
sincerajn <sincerajn@outlook.com>
2025-08-23 04:13:56 +0800

    feat: Builtin feature to pick up modules

--- ThingComps/CompTraitModule.cs
+++ ThingComps/CompTraitModule.cs
@@ -1,7 +1,7 @@
-using System.Text;
 using RimWorld;
 using UnityEngine;
 using Verse;
+using Verse.AI;
 
 namespace CWF;
 
@@ -54,5 +54,28 @@ public class CompTraitModule : ThingComp {
         }
 
         return effectLines;
+    }
+
+    public override IEnumerable<FloatMenuOption> CompFloatMenuOptions(Pawn selPawn) {
+        foreach (var option in base.CompFloatMenuOptions(selPawn)) {
+            yield return option;
+        }
+
+        if (parent.IsForbidden(selPawn)) yield break;
+        if (!selPawn.health.capacities.CapableOf(PawnCapacityDefOf.Manipulation)) yield break;
+
+        if (!selPawn.CanReserveAndReach(parent, PathEndMode.ClosestTouch, Danger.Deadly)) {
+            yield return new FloatMenuOption("CannotReach".Translate(parent.LabelCap), null);
+            yield break;
+        }
+
+        yield return new FloatMenuOption(
+            "CWF_UI_PickUp".Translate(parent.Named("MODULE")),
+            () => {
+                var job = JobMaker.MakeJob(JobDefOf.TakeInventory, parent);
+                job.count = 1;
+                selPawn.jobs.TryTakeOrderedJob(job, JobTag.Misc);
+            }
+        );
     }
 }

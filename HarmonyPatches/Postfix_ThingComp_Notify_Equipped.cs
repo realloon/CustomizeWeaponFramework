01c91c10a6ae76c0dfb3cd3fbc4f1ed37ee22491
sincerajn <sincerajn@outlook.com>
2025-08-30 06:43:46 +0800

    feat: Patch the equip event notification

---
+++ HarmonyPatches/Postfix_ThingComp_Notify_Equipped.cs
@@ -0,0 +1,12 @@
+using HarmonyLib;
+using Verse;
+
+namespace CWF.HarmonyPatches;
+
+[HarmonyPatch(typeof(ThingComp), nameof(ThingComp.Notify_Equipped))]
+public static class Postfix_ThingComp_Notify_Equipped {
+    public static void Postfix(ThingComp __instance, Pawn pawn) {
+        var compAbilityProvider = __instance.parent.GetComp<CompAbilityProvider>();
+        compAbilityProvider?.OnEquipped(pawn);
+    }
+}

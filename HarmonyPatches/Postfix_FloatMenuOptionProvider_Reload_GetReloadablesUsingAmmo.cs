9a725a9fc39238114b4f228a4ab9cd3ef8490c27
sincerajn <sincerajn@outlook.com>
2025-08-30 22:59:06 +0800

    feat: The multi-ability component now supports reloading

---
+++ HarmonyPatches/Postfix_FloatMenuOptionProvider_Reload_GetReloadablesUsingAmmo.cs
@@ -0,0 +1,26 @@
+using HarmonyLib;
+using RimWorld;
+using RimWorld.Utility;
+using Verse;
+
+namespace CWF.HarmonyPatches;
+
+[HarmonyPatch(typeof(FloatMenuOptionProvider_Reload), "GetReloadablesUsingAmmo")]
+public static class Postfix_FloatMenuOptionProvider_Reload_GetReloadablesUsingAmmo {
+    public static IEnumerable<IReloadableComp> Postfix(IEnumerable<IReloadableComp> __result, Pawn pawn,
+        Thing clickedThing) {
+        foreach (var originalResult in __result) {
+            yield return originalResult;
+        }
+
+        var primaryEqComp = pawn.equipment?.PrimaryEq;
+        if (primaryEqComp is null) yield break;
+
+        var weapon = primaryEqComp.parent;
+        var abilityProvider = weapon.TryGetComp<CompAbilityProvider>();
+
+        if (abilityProvider is not null && abilityProvider.CanBeReloadedWith(clickedThing.def)) {
+            yield return abilityProvider;
+        }
+    }
+}

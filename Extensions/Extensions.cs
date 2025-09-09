f3cad800bdafaa36a242fecc3c09e4a89465ce14
sincerajn <sincerajn@outlook.com>
2025-09-09 21:45:50 +0800

    refactor: make extension helpers internal

--- Extensions/Extensions.cs
+++ Extensions/Extensions.cs
@@ -2,12 +2,12 @@ using System.Diagnostics.CodeAnalysis;
 
 namespace CWF.Extensions;
 
-public static class Extensions {
-    public static bool IsNullOrEmpty([NotNullWhen(false)] this string? str) {
+internal static class Extensions {
+    internal static bool IsNullOrEmpty([NotNullWhen(false)] this string? str) {
         return string.IsNullOrEmpty(str);
     }
 
-    public static bool IsNullOrEmpty<T>([NotNullWhen(false)] this ICollection<T>? collection) {
+    internal static bool IsNullOrEmpty<T>([NotNullWhen(false)] this ICollection<T>? collection) {
         return collection == null || collection.Count == 0;
     }
 }

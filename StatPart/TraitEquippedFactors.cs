033a893521b0bfb8af5a362ee40b3a8f03abe361
Feliperathal <felipe.santos365@gmail.com>
2025-10-14 21:43:03 -0300

    Add support to equippedStatOffsets

---
+++ StatPart/TraitEquippedFactors.cs
@@ -0,0 +1,88 @@
+﻿using CWF.Extensions;
+using RimWorld;
+using System.Text;
+using Verse;
+
+namespace CWF;
+
+public class TraitEquippedFactors : StatPart
+{
+    public override void TransformValue(StatRequest req, ref float val)
+    {
+        if (!req.HasThing || req.Thing is not Pawn pawn) return;
+
+        var weapon = pawn?.equipment?.Primary;
+        if (weapon == null || weapon.DestroyedOrNull() || weapon.def == null) return;
+
+        var traits = weapon?.GetComp<CompDynamicTraits>()?.Traits;
+        if (traits == null || traits.Count == 0) return;
+
+        var stat = parentStat;
+        float factor = 1f;
+        foreach (var trait in traits)
+        {
+            var factors = trait?.equippedStatOffsets?.Where(t => t?.stat == stat)?.ToList();
+            if (factors.IsNullOrEmpty()) continue;
+
+            foreach (var f in factors)
+                factor *= (1f + f.value);
+        }
+        if (factor != 1f) val *= factor;
+    }
+
+    public override string ExplanationPart(StatRequest req)
+    {
+        if (!req.HasThing || req.Thing is not Pawn pawn) return "";
+
+        var weapon = pawn.equipment?.Primary;
+        if (weapon == null) return "";
+
+        var traits = weapon?.GetComp<CompDynamicTraits>()?.Traits;
+        if (traits == null || traits.Count == 0) return "";
+
+        var stat = parentStat;
+        var sb = new StringBuilder();
+        sb.AppendLine($"Weapon {weapon.Label} traits: ");
+
+        float total = 1f;
+        foreach (var t in traits)
+        {
+            var factors = t?.equippedStatOffsets?.Where(o => o.stat == stat)?.ToList();
+            if (factors == null) continue;
+
+            float local = 1f;
+            foreach (var factor in factors)
+                local *= factor.value;            
+
+            if (local != 1f)
+            {
+                total *= local;
+                sb.AppendLine($"    Trait {t.label}: x{stat.ValueToString(local)}");
+            }
+        }
+        if (total == 1f) return "";
+
+        return sb.ToString();
+    }
+
+    public override IEnumerable<Dialog_InfoCard.Hyperlink> GetInfoCardHyperlinks(StatRequest req)
+    {
+        if (!req.HasThing || req.Thing is not Pawn pawn) yield break;
+
+        var weapon = pawn.equipment?.Primary;
+        if (weapon != null)
+        {
+            yield return new Dialog_InfoCard.Hyperlink(weapon);
+        }
+
+        var traits = weapon?.GetComp<CompDynamicTraits>()?.Traits;
+        if (traits == null) yield break;
+
+        var seen = new HashSet<Def>();
+        foreach (var t in traits)
+        {
+            if (t == null || !seen.Add(t) || !TraitModuleDatabase.TryGetModuleDef(t, out ThingDef? traitDef)) continue;
+            yield return new Dialog_InfoCard.Hyperlink(traitDef);
+        }
+    }
+}

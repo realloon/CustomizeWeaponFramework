a34ada755fef1f4a8978fe82963578a7a434af2f
sincerajn <sincerajn@outlook.com>
2025-10-26 04:31:40 +0800

    style: Unified code style

--- HarmonyPatches/Postfix_TingComp_Notify_Unequipped.cs
+++ HarmonyPatches/Postfix_TingComp_Notify_Unequipped.cs
@@ -1,11 +1,12 @@
 using HarmonyLib;
 using Verse;
 
+// ReSharper disable InconsistentNaming
+
 namespace CWF.HarmonyPatches;
 
 [HarmonyPatch(typeof(ThingComp), nameof(ThingComp.Notify_Unequipped))]
 public static class Postfix_ThingComp_Notify_Unequipped {
-    // ReSharper disable once InconsistentNaming
     public static void Postfix(ThingComp __instance, Pawn pawn) {
         var compAbilityProvider = __instance.parent.GetComp<CompAbilityProvider>();
         compAbilityProvider?.OnUnequipped(pawn);

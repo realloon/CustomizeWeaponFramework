90269dd920f9844538bf8469ab5591cf79bd2ad5
sincerajn <sincerajn@outlook.com>
2025-11-02 20:47:05 +0800

    chore: annotate non-standard naming with ReSharper magic comments

--- Data/Types/ConditionalPartModifier.cs
+++ Data/Types/ConditionalPartModifier.cs
@@ -5,9 +5,11 @@ namespace CWF;
 [UsedImplicitly]
 public class ConditionalPartModifier {
     [UsedImplicitly]
+    // ReSharper disable once InconsistentNaming
     public readonly WeaponMatcher? matcher;
 
     [UsedImplicitly]
+    // ReSharper disable once InconsistentNaming
     public readonly List<Part> enablesParts = [];
 
     [UsedImplicitly]

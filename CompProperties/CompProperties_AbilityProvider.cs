453da85458385951d704aa3fb16081a5130eae24
sincerajn <sincerajn@outlook.com>
2025-11-16 13:25:55 +0800

    docs: update summary

--- CompProperties/CompProperties_AbilityProvider.cs
+++ CompProperties/CompProperties_AbilityProvider.cs
@@ -3,8 +3,7 @@ using Verse;
 namespace CWF;
 
 /// <summary>
-/// Properties class that enables a ThingDef to host the `CompAbilityProvider` component.
-/// This component is responsible for granting temporary abilities to a pawn when they equip the weapon.
+/// Properties class that enables a ThingDef to host the `CompAbilityProvider` component. This component is responsible for granting temporary abilities to a pawn when they equip the weapon.
 /// </summary>
 // ReSharper disable once InconsistentNaming
 public class CompProperties_AbilityProvider : CompProperties {

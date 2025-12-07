8bc017b29a2e4cd02f969b560a354e8b6d1ec85e
sincerajn <sincerajn@outlook.com>
2025-12-07 22:29:53 +0800

    feat: Added a "Craft" button

--- Windows/ModuleBrowserWindow.cs
+++ Windows/ModuleBrowserWindow.cs
@@ -1,11 +1,14 @@
 using System.Text;
 using UnityEngine;
+using RimWorld;
 using Verse;
+using Verse.Sound;
 
 namespace CWF;
 
 public class ModuleBrowserWindow : Window {
     private readonly Dictionary<PartDef, List<ThingDef>> _groupedModules = new();
+    private readonly Dictionary<ThingDef, RecipeDef> _recipeCache = new();
     private PartDef? _selectedPart;
     private Vector2 _rightColumnScrollPosition = Vector2.zero;
 
@@ -20,7 +23,8 @@ public class ModuleBrowserWindow : Window {
         forcePause = true;
 
         var compatibleModules = ModuleDatabase.AllModuleDefs
-            .Where(moduleDef => moduleDef.IsCompatibleWith(weapon.def));
+            .Where(moduleDef => moduleDef.IsCompatibleWith(weapon.def))
+            .ToArray();
 
         foreach (var moduleDef in compatibleModules) {
             var part = moduleDef.GetModExtension<TraitModuleExtension>().part;
@@ -30,6 +34,16 @@ public class ModuleBrowserWindow : Window {
             }
 
             _groupedModules[part].Add(moduleDef);
+        }
+
+        var relevantModules = compatibleModules.ToHashSet();
+        foreach (var recipe in DefDatabase<RecipeDef>.AllDefsListForReading) {
+            if (recipe.products.Empty()) continue;
+
+            var productDef = recipe.products[0].thingDef;
+            if (relevantModules.Contains(productDef)) {
+                _recipeCache.TryAdd(productDef, recipe);
+            }
         }
 
         _groupedModules = _groupedModules
@@ -121,27 +135,71 @@ public class ModuleBrowserWindow : Window {
             var rowRect = new Rect(contentRect.x, currentY, contentRect.width, rowHeight);
             Widgets.DrawHighlightIfMouseover(rowRect);
 
+            const float buttonSize = 24f;
+            var buttonY = rowRect.y + (rowHeight - buttonSize) / 2f;
+
+            var infoButtonRect = new Rect(rowRect.xMax - buttonSize, buttonY, buttonSize, buttonSize);
+            var actionButtonRect = new Rect(infoButtonRect.x - buttonSize, buttonY, buttonSize, buttonSize);
+            var labelRect = new Rect(rowRect.x + padding, rowRect.y,
+                actionButtonRect.x - rowRect.x - padding, rowRect.height);
+
             var sb = new StringBuilder();
             var traitDef = moduleDef.GetModExtension<TraitModuleExtension>().weaponTraitDef;
             sb.AppendLine(moduleDef.description);
             sb.AppendInNewLine(traitDef.GetTraitEffect());
 
-            TooltipHandler.TipRegion(rowRect, sb.ToString());
-
-            var infoButtonRect = new Rect(rowRect.xMax - rowHeight, rowRect.y, rowHeight, rowHeight);
-            var labelRect = new Rect(rowRect.x + padding, rowRect.y,
-                rowRect.width - padding - rowHeight - padding, rowRect.height);
+            TooltipHandler.TipRegion(labelRect, sb.ToString());
 
             UIKit.WithStyle(() => Widgets.Label(labelRect, moduleDef.LabelCap), anchor: TextAnchor.MiddleLeft);
 
-            if (Widgets.ButtonImage(infoButtonRect.ContractedBy(4f), TexButton.Info)) {
-                Find.WindowStack.Add(new Dialog_InfoCard(moduleDef));
+            currentY += rowHeight;
+
+            if (!Mouse.IsOver(rowRect)) continue;
+
+            // craft
+            if (_recipeCache.ContainsKey(moduleDef) &&
+                Widgets.ButtonImage(actionButtonRect, TexButton.Add, tooltip: "CWF_UI_Craft".Translate())) {
+                TryAddCraftingBill(moduleDef);
             }
 
-            currentY += rowHeight;
+            // info
+            if (Widgets.ButtonImage(infoButtonRect.ContractedBy(2f),
+                    TexButton.Info, tooltip: "DefInfoTip".Translate())) {
+                Find.WindowStack.Add(new Dialog_InfoCard(moduleDef));
+            }
         }
 
         Widgets.EndScrollView();
         listing.End();
+    }
+
+    private void TryAddCraftingBill(ThingDef moduleDef) {
+        if (!_recipeCache.TryGetValue(moduleDef, out var recipe)) return;
+
+        if (recipe == null) {
+            Log.Warning("Cannot craft this module.");
+            return;
+        }
+
+        var bench = Find.CurrentMap.listerBuildings.allBuildingsColonist
+            .OfType<IBillGiver>()
+            .FirstOrDefault(b => b is Thing thing && (thing.def.AllRecipes?.Contains(recipe) ?? false));
+
+        if (bench == null) {
+            Messages.Message("CWF_Message_NoWorkbenchToCraftModule".Translate(moduleDef.Named("MODULE")),
+                MessageTypeDefOf.RejectInput, false);
+            return;
+        }
+
+        var bill = recipe.MakeNewBill();
+        if (bill is Bill_Production billProduction) {
+            billProduction.repeatMode = BillRepeatModeDefOf.RepeatCount;
+            billProduction.repeatCount = 1;
+            bench.BillStack.AddBill(bill);
+        }
+
+        SoundDefOf.Click.PlayOneShotOnCamera();
+        Messages.Message("CWF_Message_BillAdded".Translate(moduleDef.Named("MODULE"), bench.Named("BENCH")),
+            new LookTargets((Thing)bench), MessageTypeDefOf.PositiveEvent);
     }
 }
